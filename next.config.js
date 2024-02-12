7dc5a8b6568485a1183f99032e46e8df32da6ff7
Hussain sihor <sihorwalahussain@gmail.com>
2024-02-12 22:33:21 +0530

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+	reactStrictMode: false,
 };
 
-module.exports = nextConfig;
+module.exports = {
+	eslint: {
+		dirs: ["pages", "utils"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+	},
+};
